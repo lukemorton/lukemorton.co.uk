92310dc50d69b9d10fc3bcab5e2db6dc4e70ec20
Luke Morton <luke@madebymade.co.uk>
2015-07-19 22:11:19 +0100

    Add formatted title that looks better on mobile

--- src/View/Model/IndexViewModel.php
+++ src/View/Model/IndexViewModel.php
@@ -10,6 +10,7 @@ class IndexViewModel extends AbstractViewModel
 	{
 		return [
 			'title' => 'An Exploration of the Web',
+			'formatted_title' => '<span>An Exploration</span> of the Web',
 			'about_url' => $data['about_url'],
 			'twitter' => $this->twitter(),
 			'thoughts' => $this->thoughts($data['markdown'], $data['posts']),
