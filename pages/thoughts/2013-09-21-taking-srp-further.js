9cec583f1f581d1c01ef87ca355ceb220b4be03e
Luke Morton <lukemorton.dev@gmail.com>
2017-01-02 20:25:43 +0000

    Make thought pages as minimal as possible

--- pages/thoughts/2013-09-21-taking-srp-further.js
+++ pages/thoughts/2013-09-21-taking-srp-further.js
@@ -1,25 +1,4 @@
 import React from 'react'
-import Page from '../../components/Page'
-import Thought from '../../components/Thought'
-import thoughts from '../../data/thoughts'
+import createThoughtPage from './_createThoughtPage'
 
-export default class extends React.Component {
-  static getInitialProps () {
-    return {
-      indexUrl: '/',
-      aboutUrl: '/about',
-      avatarSrc: 'https://s.gravatar.com/avatar/e7f62d126dec76b03e6d2393e44247ad?s=180',
-      thought: thoughts['2013-09-21-taking-srp-further']
-    }
-  }
-
-  render () {
-    return (
-      <Page title={this.props.plainTitle}>
-        <main>
-          <Thought {...this.props} />
-        </main>
-      </Page>
-    )
-  }
-}
+export default createThoughtPage('2013-09-21-taking-srp-further')
