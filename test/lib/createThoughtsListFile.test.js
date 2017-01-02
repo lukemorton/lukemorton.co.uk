7d9ee91c2b72968cac98256fa30fbda3210f0ed7
Luke Morton <lukemorton.dev@gmail.com>
2017-01-02 16:40:22 +0000

    Implement createThoughtsListFile

---
+++ test/lib/createThoughtsListFile.test.js
@@ -0,0 +1,27 @@
+import test from 'ava'
+import fs from 'fs'
+import path from 'path'
+import createThoughtsListFile from '../../lib/createThoughtsListFile'
+
+function deleteIfExists(filename) {
+  try { fs.unlinkSync(expectedFilename) } catch (e) {}
+}
+
+test('creates file in correct place', async t => {
+  const expectedFilename = path.normalize(__dirname + '/../../data/test.json')
+  deleteIfExists(expectedFilename)
+
+  const filename = await createThoughtsListFile({ name: 'test' })
+
+  t.is(filename, expectedFilename)
+  t.true(fs.existsSync(expectedFilename))
+})
+
+test('can be limited', async t => {
+  deleteIfExists(__dirname + '/../../data/limitedTest.json')
+
+  const filename = await createThoughtsListFile({ name: 'limitedTest', limit: 2 })
+
+  const parsedJson = JSON.parse(fs.readFileSync(filename, 'utf8'))
+  t.is(parsedJson.length, 2)
+})
