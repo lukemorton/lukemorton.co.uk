14b9ae81d6ba774945a6eaa982b6358e006de880
Luke Morton <lukemorton.dev@gmail.com>
2017-01-02 19:46:20 +0000

    Add first post

---
+++ pages/thoughts/2013-09-21-taking-srp-further.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'next/link'
+import Page from '../../components/Page'
+import Thought from '../../components/Thought'
+import thoughts from '../../data/thoughts'
+
+export default class extends React.Component {
+  static async getInitialProps () {
+    return {
+      indexUrl: '/',
+      aboutUrl: '/about',
+      avatarSrc: 'https://s.gravatar.com/avatar/e7f62d126dec76b03e6d2393e44247ad?s=180',
+      thought: thoughts['2013-09-21-taking-srp-further']
+    }
+  }
+
+  render () {
+    return (
+      <Page title={this.props.plainTitle}>
+        <main>
+          <Thought {...this.props} />
+        </main>
+      </Page>
+    )
+  }
+}
