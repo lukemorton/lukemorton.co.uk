8084cd95b22e4f8393dd2bc7e85695da8561c947
Luke Morton <lukemorton.dev@gmail.com>
2017-01-02 18:32:40 +0000

    Add url to thoughts

--- test/lib/createThought.test.js
+++ test/lib/createThought.test.js
@@ -4,13 +4,14 @@ import createThought from '../../lib/createThought'
 
 test('includes attributes', t => {
   const thought = createThought({
-    slug: '2016-01-01-cool-things.md',
+    slug: '2016-01-01-cool-things',
     content: fs.readFileSync(__dirname + '/mockContent.md', { encoding: 'utf8' })
   })
 
   t.is(thought.title, 'A title')
   t.is(thought.intro, '<p>An introduction\ncool.</p>\n')
-  t.is(thought.slug, '2016-01-01-cool-things.md')
+  t.is(thought.slug, '2016-01-01-cool-things')
+  t.is(thought.url, '/thoughts/2016-01-01-cool-things')
   t.regex(thought.publishedAt, /[\d]{1,2}(?:st|nd|rd|th) [A-Za-z]+ [\d]{4}/)
   t.is(thought.content.indexOf('<h1 id="a-span-title-span-">A <span>title</span></h1>'), 0)
 })
