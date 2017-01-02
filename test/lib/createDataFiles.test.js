f500005d34f9546238a0d955f9a8d29d539ca553
Luke Morton <lukemorton.dev@gmail.com>
2017-01-02 19:58:03 +0000

    Wrap array of promises in single promise

--- test/lib/createDataFiles.test.js
+++ test/lib/createDataFiles.test.js
@@ -1,21 +1,24 @@
 import test from 'ava'
 import fs from 'fs'
+import path from 'path'
 import createDataFiles from '../../lib/createDataFiles'
 
 function deleteIfExists (filename) {
-  try { fs.unlinkSync(expectedFilename) } catch (e) {}
+  try { fs.unlinkSync(filename) } catch (e) {}
 }
 
 test('creates files', async t => {
-  const thoughts = __dirname + '/../../data/thoughts.json'
-  const latestThoughts = __dirname + '/../../data/latestThoughts.json'
-  const thoughtsArchive = __dirname + '/../../data/thoughtsArchive.json'
+  const thoughts = path.join(__dirname, '../../data/thoughts.json')
+  const latestThoughts = path.join(__dirname, '../../data/latestThoughts.json')
+  const thoughtsArchive = path.join(__dirname, '../../data/thoughtsArchive.json')
 
   deleteIfExists(thoughts)
   deleteIfExists(latestThoughts)
   deleteIfExists(thoughtsArchive)
 
-  t.is(createDataFiles().length, 3)
+  const filenames = await createDataFiles()
+
+  t.is(filenames.length, 3)
   t.true(fs.existsSync(thoughts))
   t.true(fs.existsSync(latestThoughts))
   t.true(fs.existsSync(thoughtsArchive))
