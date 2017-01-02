7e1967ddc481f355d0606c70525b8c0aeca2aba1
Luke Morton <lukemorton.dev@gmail.com>
2017-01-02 18:55:13 +0000

    Implement thoughts archive

---
+++ pages/thoughts/archive.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Link from 'next/link'
+import Page from '../../components/Page'
+import Thoughts from '../../components/Thoughts'
+import thoughtsArchive from '../../data/thoughtsArchive'
+
+export default class extends React.Component {
+  static async getInitialProps () {
+    return {
+      indexUrl: '/',
+      aboutUrl: '/about',
+      avatarSrc: 'https://s.gravatar.com/avatar/e7f62d126dec76b03e6d2393e44247ad?s=180',
+      thoughts: thoughtsArchive,
+    }
+  }
+
+  render () {
+    return (
+      <Page title='An Exploration of the Web'>
+        <main>
+          <h1>
+            Thought Archive
+          </h1>
+
+          <p className='meta'>
+            <Link href={this.props.indexUrl}>
+              <img
+                src={this.props.avatarSrc}
+                style={{ height: '3em', width: '3em' }}
+                />
+            </Link>
+
+            <em>By <Link href={this.props.aboutUrl}>Luke Morton</Link></em>
+          </p>
+
+          <Thoughts
+            thoughtTitleWrapper={(title) => <h2>{title}</h2>}
+            thoughts={this.props.thoughts}
+            after={<p>Feel free to go home now <a href={this.props.indexUrl}>here</a>.</p>}
+            />
+        </main>
+      </Page>
+    )
+  }
+}
