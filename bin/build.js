da10cc73121a74631072f4cf2bfc27c7ab1e9dba
Luke Morton <lukemorton.dev@gmail.com>
2017-01-04 21:33:54 +0000

    Fix build command

--- bin/build.js
+++ bin/build.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 
 const path = require('path')
-const createDataFiles = require(path.join(__dirname, '../lib/createDataFiles'))
-const files = createDataFiles()
-console.log('Files created', files)
+const createDataFiles = require(path.join(__dirname, '../lib/createDataFiles')).default
+createDataFiles().then(files => {
+  console.log('Files built:')
+  console.log(' ', files.join('\n  '))
+})
