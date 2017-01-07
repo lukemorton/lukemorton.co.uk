1ac9df86d48dcc2f7f0fca80493e071fecd107fc
Luke Morton <lukemorton.dev@gmail.com>
2017-01-07 18:10:35 +0000

    Update build process to precompile using babel

--- bin/build.js
+++ bin/build.js
@@ -1,7 +1,8 @@
 #! /usr/bin/env node
 
-const path = require('path')
-const createDataFiles = require(path.join(__dirname, '../lib/createDataFiles')).default
+import path from 'path'
+import createDataFiles from '../lib/createDataFiles'
+
 createDataFiles().then(files => {
   console.log('Files built:')
   console.log(' ', files.join('\n  '))
