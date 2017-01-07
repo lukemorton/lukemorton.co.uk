756be528bc9012fcc872c3e995a8d4054c15e87c
Luke Morton <lukemorton.dev@gmail.com>
2017-01-07 15:41:01 +0000

    Remove unused props

--- pages/about.js
+++ pages/about.js
@@ -7,12 +7,7 @@ export default class extends React.Component {
     return {
       indexUrl: '/',
       aboutUrl: '/about',
-      archiveUrl: '/thoughts/archive',
-      twitterUrl: 'https://twitter.com/lukemorton',
-      avatarSrc: 'https://s.gravatar.com/avatar/e7f62d126dec76b03e6d2393e44247ad?s=180',
-      twitterHandle: '@LukeMorton',
-      thoughts: [],
-      projects: []
+      avatarSrc: 'https://s.gravatar.com/avatar/e7f62d126dec76b03e6d2393e44247ad?s=180'
     }
   }
 
