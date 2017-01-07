7fe17f48f9b5e6598e11d52428f4e25d5187059c
Luke Morton <lukemorton.dev@gmail.com>
2017-01-07 16:34:23 +0000

    Add extensive createServer tests

--- test/lib/createServer.test.js
+++ test/lib/createServer.test.js
@@ -1,13 +1,49 @@
 import test from 'ava'
 import supertest from 'supertest'
 import createServer from '../../lib/createServer'
+import thoughts from '../../data/thoughts'
 
-function server() {
-  const app = () => null
-  const handle = () => null
+function server({ app, handle } = {}) {
+  handle = handle || ((req, res) => res.send())
   return createServer({ app, handle })
 }
 
+function firstThought() {
+  const slug = Object.keys(thoughts)[0]
+  return thoughts[slug]
+}
+
+test('redirects non-www to www', async t => {
+  const { headers } = await supertest(server()).get('/').set('Host', 'lukemorton.co.uk').expect(302)
+  t.is(headers.location, 'https://www.lukemorton.co.uk')
+})
+
 test('serves favicon', async t => {
    await supertest(server()).get('/favicon.ico').expect(200)
+})
+
+test('serves poems', async t => {
+   await supertest(server()).get('/poems/').expect(200)
+   await supertest(server()).get('/poems/a-soul').expect(200)
+   await supertest(server()).get('/poems/a-soul.html').expect(200)
+})
+
+test('serves moon and fate', async t => {
+   await supertest(server()).get('/moon-and-fate.html').expect(200)
+})
+
+test('serves thoughts', async t => {
+  const app = { render (req, res) { return res.send() } }
+  const { url } = firstThought()
+  await supertest(server({ app })).get(url).expect(200)
+})
+
+test('serves 404 if thought does not exist', async t => {
+  const handle = (req, res) => res.status(404).send()
+  await supertest(server({ handle })).get('/thoughts/doesnt-exist').expect(404)
+})
+
+test('serves next.js by default', async t => {
+  const handle = (req, res) => res.status(200).send()
+  await supertest(server({ handle })).get('/cool').expect(200)
 })
