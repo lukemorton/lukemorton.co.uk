959fac9aa025cf94ce669eb6a89384425471dfba
Luke Morton <lukemorton.dev@gmail.com>
2017-01-08 14:16:22 +0000

    Add site verification for better GA

--- components/Page/index.js
+++ components/Page/index.js
@@ -7,8 +7,11 @@ export default ({ title, backgroundImageSrc, children }) =>
   <div className='page'>
     <Head>
       <title>{title} – Luke Morton</title>
+
       <meta charset='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta name='google-site-verification' content='lzxFltmFH5mp8y-5rFiNDfH9qQN8rHw_1zcwFVWQl7A' />
+
       <link rel='stylesheet' href='/static/reset.css' />
       <link rel='stylesheet' href='/static/screen.css' />
     </Head>
