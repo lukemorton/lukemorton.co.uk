bf45972f5e25b00a7ca7618ab786447b9b5361d9
Luke Morton <lukemorton.dev@gmail.com>
2019-12-22 10:46:29 +0000

    Rename “chapters” to “menu”

--- components/Chapters/Nav.js
+++ components/Chapters/Nav.js
@@ -3,7 +3,7 @@ import Chapter from './Chapter'
 
 export default ({ chapters }) =>
   <nav className='navigation' id='navigation'>
-    <div className='h3'>Chapters</div>
+    <div className='h3'>Menu</div>
 
     <ol>
       {chapters.map(Chapter)}
