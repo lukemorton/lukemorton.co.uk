6c45628671e7c68a516baf3c02c8029b1f60f8e2
Luke Morton <lukemorton.dev@gmail.com>
2019-12-22 13:14:34 +0000

    Add reloading of blog content in development

--- pages/thoughts/[slug].js
+++ pages/thoughts/[slug].js
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useInterval from '@use-it/interval'
 import Page from '../../components/Page'
 import Thought from '../../components/Thought'
-import { thought } from '../../src/listThoughts'
+import { thought as loadThought } from '../../src/listThoughts'
 
-export default class extends React.Component {
-  static async getInitialProps ({ req, query }) {
-    return {
-      indexUrl: '/',
-      aboutUrl: '/about',
-      archiveUrl: '/thoughts/archive',
-      avatarSrc: 'https://s.gravatar.com/avatar/e7f62d126dec76b03e6d2393e44247ad?s=180',
-      thought: await thought(req, query.slug)
-    }
-  }
+const INTERVAL = process.env.NODE_ENV === 'development' ? 1000 : null
 
-  render () {
-    return (
-      <Page title={this.props.thought.title.plain}>
-        <main>
-          <Thought {...this.props} />
-        </main>
-      </Page>
-    )
+export default function ThoughtPage (props) {
+  const [thought, setThought] = useState(props.thought)
+
+  useInterval(() => {
+    (async () => {
+      setThought(await loadThought(null, props.slug))
+    })()
+  }, INTERVAL)
+
+  return (
+    <Page title={thought.title.plain}>
+      <main>
+        <Thought {...props} thought={thought} />
+      </main>
+    </Page>
+  )
+}
+
+ThoughtPage.getInitialProps = async ({ req, query }) => {
+  return {
+    indexUrl: '/',
+    aboutUrl: '/about',
+    archiveUrl: '/thoughts/archive',
+    avatarSrc: 'https://s.gravatar.com/avatar/e7f62d126dec76b03e6d2393e44247ad?s=180',
+    slug: query.slug,
+    thought: await loadThought(req, query.slug)
   }
 }
