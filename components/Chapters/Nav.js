58fc3b08810621deb22338c85bac6066b9ed5bc2
Luke Morton <lukemorton.dev@gmail.com>
2019-12-22 21:17:30 +0000

    Update navigation to include topics

--- components/Chapters/Nav.js
+++ components/Chapters/Nav.js
@@ -1,11 +1,55 @@
 import React from 'react'
-import Chapter from './Chapter'
+import Link from 'next/link'
 
-export default ({ chapters }) =>
+export default () =>
   <nav className='navigation' id='navigation'>
     <div className='h3'>Menu</div>
 
-    <ol>
-      {chapters.map(Chapter)}
-    </ol>
+    <ul>
+      <li>
+        <Link href='/'>
+          <a>
+            Introduction
+          </a>
+        </Link>
+      </li>
+
+      <li>
+        <Link href='/thoughts/archive'>
+          <a>
+            Articles
+          </a>
+        </Link>
+      </li>
+
+      <li>
+        <Link href='/about'>
+          <a>
+            About Luke
+          </a>
+        </Link>
+      </li>
+    </ul>
+
+    <div className='h3'>
+      Topics
+    </div>
+
+    <ul>
+      <li>
+        <Link href='/topics/clean-architecture'>
+          <a>
+            Clean Architecture
+          </a>
+        </Link>
+      </li>
+
+      <li>
+        <Link href='/topics/rails'>
+          <a>
+            Ruby on Rails
+          </a>
+        </Link>
+      </li>
+    </ul>
   </nav>
