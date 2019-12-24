b875d839d6f8af95df35d0753a94577684c430eb
Luke Morton <lukemorton.dev@gmail.com>
2019-12-24 11:27:34 +0000

    Add tests for getInitialProps

--- test/pages/index.test.js
+++ test/pages/index.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import nock from 'nock'
 import Index from '../../pages/index'
 import Page from '../../components/Page'
 import Thoughts from '../../components/Thoughts'
@@ -8,4 +9,13 @@ test('content renders', () => {
   const page = shallow(<Index />)
   expect(page.find(Page).length).toBe(1)
   expect(page.find(Thoughts).length).toBe(1)
+})
+
+test('loading thoughts', async () => {
+  const expectedThoughts = [{ slug: 'cool' }, { slug: 'bob' }]
+  nock('http://lvh.me:3000')
+    .get('/dist/thoughts/latestThoughts.json')
+    .reply(200, expectedThoughts)
+  const { thoughts } = await Index.getInitialProps({})
+  expect(thoughts).toStrictEqual(expectedThoughts)
 })
