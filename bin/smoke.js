0d375f56bfb71d2100c491dc989242efaef3428c
Luke Morton <lukemorton.dev@gmail.com>
2019-12-24 13:48:54 +0000

    Fix smoke and deploy

--- bin/smoke.js
+++ bin/smoke.js
@@ -2,14 +2,8 @@
 
 const fetch = require('cross-fetch')
 const event = require(process.env.GITHUB_EVENT_PATH)
-const environment = event.deployment_status.environment
 const state = event.deployment_status.state
 const targetUrl = event.deployment_status.target_url
-
-if (environment !== 'production') {
-  console.log('NOOP: not production')
-  process.exit(1)
-}
 
 if (state === 'success') {
   console.log('Smoke testing successful deploy')
