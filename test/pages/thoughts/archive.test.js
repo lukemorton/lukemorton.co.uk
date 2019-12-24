bc0a053623d4b4698c5a554dc9477a5167bdd204
Luke Morton <lukemorton.dev@gmail.com>
2019-12-24 11:27:34 +0000

    Add tests for getInitialProps

--- test/pages/thoughts/archive.test.js
+++ test/pages/thoughts/archive.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import nock from 'nock'
 import ThoughtArchive from '../../../pages/thoughts/archive'
 import Page from '../../../components/Page'
 import Thoughts from '../../../components/Thoughts'
@@ -8,4 +9,13 @@ test('content renders', () => {
   const page = shallow(<ThoughtArchive />)
   expect(page.find(Page).length).toBe(1)
   expect(page.find(Thoughts).length).toBe(1)
+})
+
+test('loading thoughts', async () => {
+  const expectedThoughts = [{ slug: 'cool' }, { slug: 'bob' }]
+  nock('http://lvh.me:3000')
+    .get('/dist/thoughts/thoughtsArchive.json')
+    .reply(200, expectedThoughts)
+  const { thoughts } = await ThoughtArchive.getInitialProps({})
+  expect(thoughts).toStrictEqual(expectedThoughts)
 })
