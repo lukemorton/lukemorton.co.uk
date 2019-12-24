29f851797c0dbdf09c836ed2e8e98f88983bc973
Luke Morton <lukemorton.dev@gmail.com>
2019-12-24 13:31:41 +0000

    Halt production deploy if not production success

--- bin/smoke.js
+++ bin/smoke.js
@@ -2,8 +2,14 @@
 
 const fetch = require('cross-fetch')
 const event = require(process.env.GITHUB_EVENT_PATH)
+const environment = event.deployment_status.environment
 const state = event.deployment_status.state
 const targetUrl = event.deployment_status.target_url
+
+if (environment !== 'production') {
+  console.log('NOOP: not production')
+  process.exit(1)
+}
 
 if (state === 'success') {
   console.log('Smoke testing successful deploy')
@@ -17,5 +23,6 @@ if (state === 'success') {
       process.exit(1)
     })
 } else {
-  console.log('NOOP')
+  console.log('NOOP: Ignoring non-success event')
+  process.exit(1)
 }
