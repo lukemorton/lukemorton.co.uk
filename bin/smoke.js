d670c71112eecfcc38555b2345220a185c9df52b
Luke Morton <lukemorton.dev@gmail.com>
2019-12-24 12:34:47 +0000

    Add bin/smoke.js

---
+++ bin/smoke.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const fetch = require('cross-fetch')
+const event = require(process.env.GITHUB_EVENT_PATH)
+const state = event.deployment_status.state
+const targetUrl = event.deployment_status.target_url
+
+if (state === 'success') {
+  console.log('Smoke testing successful deploy')
+  console.log(`URL: ${targetUrl}`)
+  fetch(targetUrl)
+    .then(res => {
+      console.log('Status:', res.status)
+    })
+    .catch(err => {
+      console.error(err)
+      process.exit(1)
+    })
+} else {
+  console.log('NOOP')
+}
