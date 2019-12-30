15ba5309c2cd7e09377074667467eb5768a2b416
Luke Morton <lukemorton.dev@gmail.com>
2019-12-30 00:04:22 +0000

    Replace specific topic files for topics/[slug]

---
+++ src/topics.js
@@ -0,0 +1,19 @@
+export const TOPICS = [
+  { name: 'Clean Architecture', slug: 'clean-architecture', file: 'cleanArchitecture' },
+  { name: 'Ruby on Rails', slug: 'rails', file: 'rails' },
+]
+
+export const TOPIC_SLUG_TO_FILE_MAP = {
+  'clean-architecture': 'cleanArchitecture',
+  'rails': 'rails'
+}
+
+export const TOPIC_SLUG_TO_NAME_MAP = {
+  'clean-architecture': 'Clean Architecture',
+  'rails': 'Ruby on Rails'
+}
+
+export const TOPIC_NAME_TO_FILE_MAP = {
+  'Clean Architecture': 'cleanArchitecture',
+  'Ruby on Rails': 'rails'
+}
