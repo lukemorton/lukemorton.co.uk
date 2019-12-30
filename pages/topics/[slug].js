5479bad4b0c1bfc8f616751234bb260ded3e9ff2
Luke Morton <lukemorton.dev@gmail.com>
2019-12-30 15:58:20 +0000

    Use function rather than class

--- pages/topics/[slug].js
+++ pages/topics/[slug].js
@@ -6,35 +6,33 @@ import { fetchTopicBySlug } from '../../src/fetchTopic'
 import { fetchThoughtsByTopicSlug } from '../../src/fetchThoughts'
 import withCommonProps from '../../src/withCommonProps'
 
-export default class extends React.Component {
-  static async getInitialProps ({ req, query }) {
-    return withCommonProps({
-      topic: fetchTopicBySlug(query.slug),
-      thoughts: await fetchThoughtsByTopicSlug(req, query.slug)
-    })
-  }
+export default function Topic ({ indexUrl, topic, thoughts }) {
+  return (
+    <Page title={`Articles on ${topic.name}`}>
+      <main>
+        <div className='prose'>
+          <h1>
+            {topic.name}
+          </h1>
 
-  render () {
-    return (
-      <Page title={`Articles on ${this.props.topic.name}`}>
-        <main>
-          <div className='prose'>
-            <h1>
-              {this.props.topic.name}
-            </h1>
+          <p>
+            How cool!
+          </p>
+        </div>
 
-            <p>
-              How cool!
-            </p>
-          </div>
+        <Thoughts
+          thoughtTitleWrapper={(title) => <h2 className='h3'>{title}</h2>}
+          thoughts={thoughts}
+          after={<p>Feel free to go home now <Link href={indexUrl}><a>here</a></Link>.</p>}
+          />
+      </main>
+    </Page>
+  )
+}
 
-          <Thoughts
-            thoughtTitleWrapper={(title) => <h2 className='h3'>{title}</h2>}
-            thoughts={this.props.thoughts}
-            after={<p>Feel free to go home now <Link href={this.props.indexUrl}><a>here</a></Link>.</p>}
-            />
-        </main>
-      </Page>
-    )
-  }
+Topic.getInitialProps = async ({ req, query }) => {
+  return withCommonProps({
+    topic: fetchTopicBySlug(query.slug),
+    thoughts: await fetchThoughtsByTopicSlug(req, query.slug)
+  })
 }
