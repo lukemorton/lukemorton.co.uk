32512156dd59afeb17d3826571243a2285780abf
Luke Morton <lukemorton.dev@gmail.com>
2019-12-30 14:59:23 +0000

    Use fetchTopicBySlug

--- pages/topics/[slug].js
+++ pages/topics/[slug].js
@@ -2,25 +2,25 @@ import React from 'react'
 import Link from 'next/link'
 import Page from '../../components/Page'
 import Thoughts from '../../components/Thoughts'
+import { fetchTopicBySlug } from '../../src/fetchTopic'
 import { fetchThoughtsByTopicSlug } from '../../src/fetchThoughts'
-import { TOPIC_SLUG_TO_NAME_MAP } from '../../src/topics'
 import withCommonProps from '../../src/withCommonProps'
 
 export default class extends React.Component {
   static async getInitialProps ({ req, query }) {
     return withCommonProps({
-      topicName: TOPIC_SLUG_TO_NAME_MAP[query.slug],
+      topic: fetchTopicBySlug(query.slug),
       thoughts: await fetchThoughtsByTopicSlug(req, query.slug)
     })
   }
 
   render () {
     return (
-      <Page title={`Articles on ${this.props.topicName}`}>
+      <Page title={`Articles on ${this.props.topic.name}`}>
         <main>
           <div className='prose'>
             <h1>
-              {this.props.topicName}
+              {this.props.topic.name}
             </h1>
 
             <p>
