b3c910233ae38a436125602c5e78aa3efca9145c
Luke Morton <lukemorton.dev@gmail.com>
2019-12-30 10:38:46 +0000

    Add TEST_ORIGIN

--- test/pages/index.test.js
+++ test/pages/index.test.js
@@ -13,7 +13,7 @@ test('content renders', () => {
 
 test('loading thoughts', async () => {
   const expectedThoughts = [{ slug: 'cool' }, { slug: 'bob' }]
-  nock('http://lvh.me:3000')
+  nock(TEST_ORIGIN)
     .get('/dist/thoughts/recent.json')
     .reply(200, expectedThoughts)
   const { thoughts } = await Index.getInitialProps({})
