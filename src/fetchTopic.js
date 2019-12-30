524494342159f92bbbac7fba2f6b9c14117ccc10
Luke Morton <lukemorton.dev@gmail.com>
2019-12-30 19:41:26 +0000

    Improve errors

--- src/fetchTopic.js
+++ src/fetchTopic.js
@@ -1,9 +1,15 @@
 import { findTopicBySlug } from './topicGateway'
 
-export class NoTopicFoundBySlugError extends Error {}
+export class NoTopicFoundBySlugError extends Error {
+  constructor (slug) {
+    super()
+    this.name = 'NoTopicFoundBySlugError'
+    this.message = `Could not find topic by slug ${slug}`
+  }
+}
 
-export function fetchTopicBySlug (name) {
-  const topic = findTopicBySlug(name)
-  if (!topic) throw new NoTopicFoundBySlugError()
+export function fetchTopicBySlug (slug) {
+  const topic = findTopicBySlug(slug)
+  if (!topic) throw new NoTopicFoundBySlugError(slug)
   return topic
 }
