384cbc1fe415941999a03512e7e6c8e8f069237b
Luke Morton <lukemorton.dev@gmail.com>
2019-12-30 20:00:24 +0000

    Add more comprehensive smoke test

--- bin/smoke.js
+++ bin/smoke.js
@@ -5,17 +5,27 @@ const event = require(process.env.GITHUB_EVENT_PATH)
 const state = event.deployment_status.state
 const targetUrl = event.deployment_status.target_url
 
-if (state === 'success') {
-  console.log('Smoke testing successful deploy')
-  console.log(`URL: ${targetUrl}`)
-  fetch(targetUrl)
+function testUrl (origin, path) {
+  const url = `${origin}${path || ''}`
+
+  fetch(url)
     .then(res => {
+      console.log(`URL: ${url}`)
       console.log('Status:', res.status)
     })
     .catch(err => {
+      console.log(`URL: ${url}`)
       console.error(err)
       process.exit(1)
     })
+}
+
+if (state === 'success') {
+  console.log('Smoke testing successful deploy')
+  testUrl(targetUrl)
+  testUrl(targetUrl, '/about')
+  testUrl(targetUrl, '/open-source')
+  testUrl(targetUrl, '/thoughts/archive')
 } else {
   console.log('NOOP: Ignoring non-success event')
   process.exit(1)
