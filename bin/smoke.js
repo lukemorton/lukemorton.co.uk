f938b0cc72bf9b3a0fa0a45821d7e3b210675854
Luke Morton <lukemorton.dev@gmail.com>
2019-12-31 02:26:17 +0000

    Improve smoke to test all thoughts

--- bin/smoke.js
+++ bin/smoke.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-const fetch = require('cross-fetch')
-const event = require(process.env.GITHUB_EVENT_PATH)
-const state = event.deployment_status.state
-const targetUrl = event.deployment_status.target_url
+import fetch from 'cross-fetch'
+import fs from 'fs'
+import { fetchAllThoughts } from '../src/fetchThoughts.js'
 
 function testUrl (origin, path) {
   const url = `${origin}${path || ''}`
@@ -20,13 +19,27 @@ function testUrl (origin, path) {
     })
 }
 
-if (state === 'success') {
-  console.log('Smoke testing successful deploy')
-  testUrl(targetUrl)
-  testUrl(targetUrl, '/about')
-  testUrl(targetUrl, '/open-source')
-  testUrl(targetUrl, '/thoughts/archive')
-} else {
-  console.log('NOOP: Ignoring non-success event')
-  process.exit(1)
-}
+(async () => {
+  const eventJson = fs.readFileSync(process.env.GITHUB_EVENT_PATH)
+  const event = JSON.parse(eventJson)
+  const state = event.deployment_status.state
+  const targetUrl = event.deployment_status.target_url
+
+  if (state === 'success') {
+    console.log('Smoke testing successful deploy')
+    testUrl(targetUrl)
+    testUrl(targetUrl, '/about')
+    testUrl(targetUrl, '/open-source')
+    testUrl(targetUrl, '/thoughts/archive')
+
+    const { host } = new URL(targetUrl)
+    const thoughts = await fetchAllThoughts({ headers: { host } })
+
+    thoughts.forEach(t => {
+      testUrl(targetUrl, `/thoughts/${t.slug}`)
+    })
+  } else {
+    console.log('NOOP: Ignoring non-success event')
+    process.exit(1)
+  }
+})()
