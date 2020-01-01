d113158a1c19fbe76983e09144a470f7a6fa5d78
Luke Morton <lukemorton.dev@gmail.com>
2020-01-01 16:59:09 +0000

    Load use case within useLiveBlog and keep page tidy

--- src/app/hooks/useLiveBlog.js
+++ src/app/hooks/useLiveBlog.js
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import useInterval from '@use-it/interval'
+import dependencyContainer from '../dependencyContainer'
 
 const INTERVAL = process.env.NODE_ENV === 'development' ? 1000 : null
 
-export default function (propThought, loadThought) {
+export default function (propThought, origin) {
   const [thought, setThought] = useState(propThought)
 
   if (propThought.slug !== thought.slug) setThought(propThought)
 
   useInterval(() => {
-    (async () => { setThought(await loadThought()) })()
+    (async () => {
+      const { fetchOneThoughtBySlug } = await dependencyContainer()
+      setThought(await fetchOneThoughtBySlug(origin, propThought.slug))
+    })()
   }, INTERVAL)
 
   return [thought, setThought]
