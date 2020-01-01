bc84bf9c119ff1dbe0c0f28b2199dae750ae0839
Luke Morton <lukemorton.dev@gmail.com>
2020-01-01 13:39:34 +0000

    Use usecase rather than gateway

--- test/pages/topics/[slug].test.js
+++ test/pages/topics/[slug].test.js
@@ -4,15 +4,15 @@ import nock from 'nock'
 import Topic from '../../../pages/topics/[slug]'
 import Page from '../../../src/app/components/Page'
 import Thoughts from '../../../src/app/components/Thoughts'
-import { allTopics } from '../../../src/blog/topicGateway'
+import { fetchAllTopics } from '../../../src/app/factory'
 
 test('content renders', () => {
-  const page = shallow(<Topic topic={allTopics()[0]} thoughts={[]} />)
+  const page = shallow(<Topic topic={fetchAllTopics()[0]} thoughts={[]} />)
   expect(page.find(Page).length).toBe(1)
   expect(page.find(Thoughts).length).toBe(1)
 })
 
-allTopics().forEach((topic) => {
+fetchAllTopics().forEach((topic) => {
   test(`loading ${topic.name} thoughts`, async () => {
     const expectedThoughts = [{ slug: 'cool' }, { slug: 'bob' }]
     nock(TEST_ORIGIN).get(topic.path).reply(200, expectedThoughts)
