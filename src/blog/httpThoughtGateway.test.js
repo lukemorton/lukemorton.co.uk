1eba12a1468b5b1de42ab6585cf1eca3c6af21b3
Luke Morton <lukemorton.dev@gmail.com>
2020-01-02 15:31:58 +0000

    Add tests for origin building

--- src/blog/httpThoughtGateway.test.js
+++ src/blog/httpThoughtGateway.test.js
@@ -5,6 +5,8 @@ jest.mock('cross-fetch')
 
 describe('httpThoughtGateway', () => {
   describe('.loadJsonPath()', () => {
+    const origin = TEST_ORIGIN
+
     beforeEach(() => {
       jest.spyOn(console, 'error').mockImplementation()
 
@@ -14,26 +16,63 @@ describe('httpThoughtGateway', () => {
     })
 
     afterEach(() => {
+      jest.resetAllMocks()
       jest.restoreAllMocks()
     })
 
     test('it uses fetch', async () => {
-      await loadJsonPath(null, '/dist/thoughts/index.json')
+      await loadJsonPath(origin, '/dist/thoughts/index.json')
       expect(fetch).toHaveBeenCalledWith(
         expect.stringContaining('/dist/thoughts/index.json')
       )
     })
 
+    test('it uses prepends origin', async () => {
+      await loadJsonPath(origin, '/dist/thoughts/index.json')
+      expect(fetch).toHaveBeenCalledWith(
+        `${origin}/dist/thoughts/index.json`
+      )
+    })
+
+    describe('using window.location object to fetch methods', () => {
+      let location
+
+      beforeAll(() => {
+        location = window.location
+        delete window.location
+      })
+
+      afterAll(() => {
+        window.location = location
+      })
+
+      test('it uses window.location for origin if null', async () => {
+        window.location = { hostname: 'lukemorton.co.uk' }
+        await loadJsonPath(null, '/dist/thoughts/index.json')
+        expect(fetch).toHaveBeenCalledWith(
+          'https://lukemorton.co.uk/dist/thoughts/index.json'
+        )
+      })
+
+      test('it replaces localhost with lvh.me', async () => {
+        window.location = { hostname: 'localhost' }
+        await loadJsonPath(null, '/dist/thoughts/index.json')
+        expect(fetch).toHaveBeenCalledWith(
+          'http://lvh.me:3000/dist/thoughts/index.json'
+        )
+      })
+    })
+
     test('it throws exception', async () => {
       const err = new Error('Uh oh')
       fetch.mockRejectedValueOnce(err)
-      await expect(loadJsonPath(null, '/dist/thoughts/index.json')).rejects.toThrow(err)
+      await expect(loadJsonPath(origin, '/dist/thoughts/index.json')).rejects.toThrow(err)
     })
 
     test('it logs exception', async () => {
       const err = new Error('Uh oh')
       fetch.mockRejectedValueOnce(err)
-      try { await loadJsonPath(null, '/dist/thoughts/index.json') } catch {}
+      try { await loadJsonPath(origin, '/dist/thoughts/index.json') } catch {}
       expect(console.error).toHaveBeenCalled()
     })
   })
