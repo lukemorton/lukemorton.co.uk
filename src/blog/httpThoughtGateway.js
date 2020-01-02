46106dd95138c18fd8df110e5e4681eab337a893
Luke Morton <lukemorton.dev@gmail.com>
2020-01-02 15:31:58 +0000

    Add tests for origin building

--- src/blog/httpThoughtGateway.js
+++ src/blog/httpThoughtGateway.js
@@ -1,6 +1,22 @@
 import fetch from 'cross-fetch'
 
+function buildOriginFromWindowIfNull (origin) {
+  if (origin) return origin
+
+  if (typeof window !== 'undefined') {
+    const host = window.location.hostname
+
+    if (host.indexOf('localhost') > -1) {
+      return 'http://lvh.me:3000'
+    } else {
+      return `https://${host}`
+    }
+  }
+}
+
 export async function loadJsonPath (origin, path) {
+  origin = buildOriginFromWindowIfNull(origin)
+
   const url = `${origin}${path}`
 
   try {
