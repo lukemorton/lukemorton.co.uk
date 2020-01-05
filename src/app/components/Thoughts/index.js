87e75f42ea2a08e0e37253fd3b1dfaae72786d9d
Luke Morton <lukemorton.dev@gmail.com>
2020-01-05 22:35:29 +0000

    Move style into child component

--- src/app/components/Thoughts/index.js
+++ src/app/components/Thoughts/index.js
@@ -2,21 +2,29 @@ import React from 'react'
 import Link from 'next/link'
 import Prose from '../Prose'
 
-const Thought = ({ title, thoughtTitleWrapper, url, slug, publishedAt, excerpt }) =>
-  <li className='post'>
+const Thought = ({ small, title, thoughtTitleWrapper, url, slug, publishedAt, excerpt }) =>
+  <li className={small ? 'post small' : 'post'}>
     <style jsx>{`
       .post {
         margin-bottom: 1.6em;
       }
 
-      h2, h3 {
-        margin-bottom: 0;
+      .post.small {
+        display: block;
+        font-size: .7em;
+        line-height: 1.8em;
+        width: 48%;
+      }
+
+      .post.small:only-child {
+        width: 100%;
       }
 
       .meta {
         color: #999;
         margin: 0.5em 0;
       }
+
     `}</style>
 
     {thoughtTitleWrapper(<Link href='/thoughts/[slug]' as={`/thoughts/${slug}`}><a>{title.plain}</a></Link>)}
@@ -36,10 +44,8 @@ const thoughtMapper = (small, thoughtTitleWrapper) => (props, i) =>
     small={small}
   />
 
-const thoughtsClass = (small) => small ? 'thoughts small' : 'thoughts'
-
 export default ({ small, title, thoughtTitleWrapper, thoughts, after }) =>
-  <div className={thoughtsClass(small)}>
+  <div className={small ? 'small' : ''}>
     <style jsx>{`
       ul {
         list-style: none;
@@ -47,21 +53,10 @@ export default ({ small, title, thoughtTitleWrapper, thoughts, after }) =>
         padding: 0;
       }
 
-      .thoughts.small ul {
+      .small ul {
         display: flex;
         flex-wrap: wrap;
         justify-content: space-between;
-      }
-
-      .thoughts.small :global(.post) {
-        display: block;
-        font-size: .7em;
-        line-height: 1.8em;
-        width: 48%;
-      }
-
-      .thoughts.small :global(.post:only-child) {
-        width: 100%;
       }
     `}</style>
 
