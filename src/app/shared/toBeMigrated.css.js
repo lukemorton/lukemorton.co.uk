d22565f0daa2ae731a4312ecf5ff0004cc0bdcb8
Luke Morton <lukemorton.dev@gmail.com>
2020-01-05 16:22:21 +0000

    Move to using styled-jsx

--- src/app/shared/toBeMigrated.css.js
+++ src/app/shared/toBeMigrated.css.js
@@ -1,100 +1,6 @@
-/* typography */
+import css from 'styled-jsx/css'
 
-body {
-  margin: 1em;
-
-  font-family: Georgia, "Times New Roman", Times, serif;
-  font-size: 16px;
-  line-height: 1.8em;
-
-  color: #333333;
-  background-color: #ffffff;
-}
-
-h1,
-h2,
-h3,
-.h3,
-h4 {
-  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-weight: bold;
-  line-height: 1.5em;
-  color: inherit;
-  text-rendering: optimizelegibility;
-}
-
-h1,
-h2,
-h3,
-.h3 {
-  margin: 1em 0 0.5em 0;
-}
-
-h1 {
-  font-size: 2.5em;
-  margin: 0 0 1rem 0;
-}
-
-h2 {
-  font-size: 2em;
-}
-
-h3,
-.h3 {
-  font-size: 1.5em;
-}
-
-h4 {
-  font-size: 1.2em;
-  margin: 0.5em 0 0.25em 0;
-}
-
-p {
-  margin: 0 0 1.6em 0;
-}
-
-ul,
-ol {
-  padding: 0;
-  margin: 0 1.6em 1.6em 1.6em;
-}
-
-li {
-  margin-bottom: 1em;
-}
-
-blockquote {
-  padding: 0 0 0 1em;
-  margin: 0 0 1.6em 0;
-  font-style: italic;
-}
-
-blockquote p {
-  font-size: 1.2em;
-}
-
-blockquote p cite {
-  font-size: .8em;
-  color: #999;
-}
-
-h2 + blockquote {
-  margin-top: 2em;
-}
-
-a,
-a code {
-  color: #0088cc;
-  text-decoration: underline;
-}
-
-a:hover,
-a:focus {
-  color: #005580;
-}
-
-/* layout */
-
+export default css.global`
 .content {
   display: flex;
   flex-direction: column-reverse;
@@ -108,15 +14,11 @@ a:focus {
 .content__secondary {
 }
 
-/* utils */
-
 .break--mobile,
 .break--tablet,
 .break--always {
   display: block;
 }
-
-/* components */
 
 .author {
   color: #666;
@@ -327,3 +229,4 @@ hr {
   -webkit-animation-name: fadeIn;
   animation-name: fadeIn;
 }
+`
