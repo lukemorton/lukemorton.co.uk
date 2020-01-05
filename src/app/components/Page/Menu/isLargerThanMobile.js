cf8567390b9f8c88bf2f99291c8612d4dafbd1eb
Luke Morton <lukemorton.dev@gmail.com>
2020-01-05 15:35:01 +0000

    Add test for related content in Thought page

--- src/app/components/Page/Menu/isLargerThanMobile.js
+++ src/app/components/Page/Menu/isLargerThanMobile.js
@@ -1,5 +1,5 @@
 export default function () {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined' && window.matchMedia) {
     const media = window.matchMedia('(min-width: 60em)')
     return media.matches
   } else {
