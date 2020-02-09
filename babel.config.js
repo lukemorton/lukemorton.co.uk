f3d23723ba6f5c953d52c9c8e34a2ed3281a9ac4
Luke Morton <lukemorton.dev@gmail.com>
2020-02-09 17:42:44 +0000

    Move remaining config into babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,48 @@
 module.exports = api => {
-  if (api.env('test')) {
-    return {
-      plugins: [
-        '@babel/plugin-transform-runtime',
-        'styled-jsx/babel'
-      ],
-      presets: [
-        '@babel/preset-env',
-        '@babel/preset-react'
-      ]
-    }
-  } else {
-    return {
-      plugins: [
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-syntax-dynamic-import',
-        'styled-jsx/babel'
-      ],
-      presets: [
-        '@babel/preset-react'
-      ]
-    }
+  api.cache(true)
+
+  switch (process.env.NODE_ENV) {
+    case 'test':
+      return {
+        plugins: [
+          '@babel/plugin-transform-runtime',
+          'styled-jsx/babel'
+        ],
+        presets: [
+          '@babel/preset-env',
+          '@babel/preset-react'
+        ]
+      }
+    case 'build:lib':
+      return {
+        ignore: [
+          'src/**/*.test.js',
+          'src/setupTests.js'
+        ],
+        plugins: [
+          '@babel/plugin-transform-runtime',
+          '@babel/plugin-transform-modules-commonjs',
+          '@babel/plugin-syntax-dynamic-import',
+          'styled-jsx/babel'
+        ],
+        presets: [
+          '@babel/preset-react'
+        ]
+      }
+    default:
+      return {
+        ignore: [
+          'src/**/*.test.js',
+          'src/setupTests.js'
+        ],
+        plugins: [
+          '@babel/plugin-transform-runtime',
+          '@babel/plugin-syntax-dynamic-import',
+          'styled-jsx/babel'
+        ],
+        presets: [
+          '@babel/preset-react'
+        ]
+      }
   }
 }
