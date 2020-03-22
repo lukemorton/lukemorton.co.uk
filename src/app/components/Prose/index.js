1f06d43d931e84813402925a448b7edcf8650241
Luke Morton <lukemorton.dev@gmail.com>
2020-03-22 13:50:34 +0000

    Add a max-width for images in prose

--- src/app/components/Prose/index.js
+++ src/app/components/Prose/index.js
@@ -1,66 +1,72 @@
-import React from 'react'
+import React from "react";
 
-export default ({ children }) =>
-  <div className='prose'>
-    <style jsx>{`
-      .prose :global(h1),
-      .prose :global(h2),
-      .prose :global(h3),
-      .prose :global(.h3) {
-        margin: 1em 0 0.5em 0;
-      }
+export default ({ children }) => (
+  <div className="prose">
+    <style jsx>
+      {`
+        .prose :global(h1),
+        .prose :global(h2),
+        .prose :global(h3),
+        .prose :global(.h3) {
+          margin: 1em 0 0.5em 0;
+        }
 
-      .prose :global(h1) {
-        margin: 0 0 1rem 0;
-      }
+        .prose :global(h1) {
+          margin: 0 0 1rem 0;
+        }
 
-      .prose :global(h4) {
-        margin: 0.5em 0 0.25em 0;
-      }
+        .prose :global(h4) {
+          margin: 0.5em 0 0.25em 0;
+        }
 
-      .prose :global(p) {
-        margin: 0 0 1.6em 0;
-      }
+        .prose :global(p) {
+          margin: 0 0 1.6em 0;
+        }
 
-      .prose :global(p:first-of-type) {
-        font-size: 1.2em;
-        margin: 1.6em 0;
-      }
+        .prose :global(p:first-of-type) {
+          font-size: 1.2em;
+          margin: 1.6em 0;
+        }
 
-      .prose :global(.meta) + :global(p) {
-        font-size: 1.2em;
-        margin: 1.6em 0;
-      }
+        .prose :global(.meta) + :global(p) {
+          font-size: 1.2em;
+          margin: 1.6em 0;
+        }
 
-      .prose :global(ul),
-      .prose :global(ol) {
-        padding: 0;
-        margin: 0 1.6em 1.6em 1.6em;
-      }
+        .prose :global(ul),
+        .prose :global(ol) {
+          padding: 0;
+          margin: 0 1.6em 1.6em 1.6em;
+        }
 
-      .prose :global(li) {
-        margin-bottom: 1em;
-      }
+        .prose :global(li) {
+          margin-bottom: 1em;
+        }
 
-      .prose :global(blockquote) {
-        padding: 0 0 0 1em;
-        margin: 0 0 1.6em 0;
-        font-style: italic;
-      }
+        .prose :global(blockquote) {
+          padding: 0 0 0 1em;
+          margin: 0 0 1.6em 0;
+          font-style: italic;
+        }
 
-      .prose :global(blockquote) :global(p) {
-        font-size: 1.2em;
-      }
+        .prose :global(blockquote) :global(p) {
+          font-size: 1.2em;
+        }
 
-      .prose :global(blockquote) :global(p) :global(cite) {
-        font-size: .8em;
-        color: #595959;
-      }
+        .prose :global(blockquote) :global(p) :global(cite) {
+          font-size: 0.8em;
+          color: #595959;
+        }
 
-      .prose :global(h2) + :global(blockquote) {
-        margin-top: 2em;
-      }
-    `}
+        .prose :global(h2) + :global(blockquote) {
+          margin-top: 2em;
+        }
+
+        .prose :global(img) {
+          max-width: 100%;
+        }
+      `}
     </style>
     {children}
   </div>
+);
