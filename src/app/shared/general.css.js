c026ba951609d6d28a7b5cf6f859247a59053c04
Luke Morton <lukemorton.dev@gmail.com>
2020-03-24 21:58:54 +0000

    Use black text and jumbo for h1

--- src/app/shared/general.css.js
+++ src/app/shared/general.css.js
@@ -2,53 +2,61 @@ import css from 'styled-jsx/css'
 import { fromMedium, fromExtraLarge } from '../settings/breakpoints'
 
 export default css.global`
-body {
-  color: #333333;
-  background-color: #ffffff;
-  margin: 1em;
-}
-
-@media ${fromMedium} {
   body {
-    margin: 2em;
+    color: #333333;
+    background-color: #ffffff;
+    margin: 1em;
   }
-}
 
-@media ${fromExtraLarge} {
-  body {
-    margin: 5em 3em;
+  @media ${fromMedium} {
+    body {
+      margin: 2em;
+    }
   }
-}
 
-a,
-a code {
-  color: #0056B3;
-  text-decoration: underline;
-}
+  @media ${fromExtraLarge} {
+    body {
+      margin: 5em 3em;
+    }
+  }
 
-a:hover,
-a:focus {
-  color: #003166;
-}
+  h1,
+  h2,
+  h3,
+  .h3,
+  h4 {
+    color: #000;
+  }
 
-h1 a,
-h2 a,
-h3 a,
-.h3 a,
-h4 a {
-  color: #0063cc;
-}
+  a,
+  a code {
+    color: #0056b3;
+    text-decoration: underline;
+  }
 
-h1 a:hover,
-h2 a:hover,
-h3 a:hover,
-.h3 a:hover,
-h4 a:hover,
-h1 a:focus,
-h2 a:focus,
-h3 a:focus,
-.h3 a:focus,
-h4 a:focus {
-  color: #004C9E;
-}
+  a:hover,
+  a:focus {
+    color: #003166;
+  }
+
+  h1 a,
+  h2 a,
+  h3 a,
+  .h3 a,
+  h4 a {
+    color: #0063cc;
+  }
+
+  h1 a:hover,
+  h2 a:hover,
+  h3 a:hover,
+  .h3 a:hover,
+  h4 a:hover,
+  h1 a:focus,
+  h2 a:focus,
+  h3 a:focus,
+  .h3 a:focus,
+  h4 a:focus {
+    color: #004c9e;
+  }
 `
