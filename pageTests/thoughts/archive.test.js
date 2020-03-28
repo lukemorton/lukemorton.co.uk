8203230216609b89da5da6b8d03db91d72b84335
Luke Morton <lukemorton.dev@gmail.com>
2020-03-28 20:14:21 +0000

    Move page tests to pageTests

--- pageTests/thoughts/archive.test.js
+++ pageTests/thoughts/archive.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Index from '../../pages/index'
+import ThoughtArchive from '../../pages/thoughts/archive'
 import Page from '../../src/app/components/Page'
 import Thoughts from '../../src/app/components/Thoughts'
 
 test('content renders', () => {
-  const page = shallow(<Index />)
+  const page = shallow(<ThoughtArchive />)
   expect(page.find(Page).length).toBe(1)
   expect(page.find(Thoughts).length).toBe(1)
 })
--- pageTests/topics/[slug].test.js
+++ pageTests/topics/[slug].test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Topic from '../../../pages/topics/[slug]'
-import Page from '../../../src/app/components/Page'
-import Thoughts from '../../../src/app/components/Thoughts'
-import { fetchAllTopics } from '../../../src/app/factory'
+import Topic from '../../pages/topics/[slug]'
+import Page from '../../src/app/components/Page'
+import Thoughts from '../../src/app/components/Thoughts'
+import { fetchAllTopics } from '../../src/app/factory'
 
 test('content renders', () => {
   const page = shallow(<Topic topic={fetchAllTopics()[0]} thoughts={[]} />)
