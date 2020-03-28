f34ea56b055789dfa34fdcd184903d007fbd2789
Luke Morton <lukemorton.dev@gmail.com>
2020-03-28 19:59:40 +0000

    Simplify babel config

--- babel.config.js
+++ babel.config.js
@@ -1,48 +1,17 @@
 module.exports = api => {
   api.cache(true)
 
+  const presets = ['next/babel']
+
+  const ignoreTests = [
+    'src/**/*.test.js',
+    'src/setupTests.js'
+  ]
+
   switch (process.env.NODE_ENV) {
     case 'test':
-      return {
-        plugins: [
-          '@babel/plugin-transform-runtime',
-          'styled-jsx/babel'
-        ],
-        presets: [
-          '@babel/preset-env',
-          '@babel/preset-react'
-        ]
-      }
-    case 'build:lib':
-      return {
-        ignore: [
-          'src/**/*.test.js',
-          'src/setupTests.js'
-        ],
-        plugins: [
-          '@babel/plugin-transform-runtime',
-          '@babel/plugin-transform-modules-commonjs',
-          '@babel/plugin-syntax-dynamic-import',
-          'styled-jsx/babel'
-        ],
-        presets: [
-          '@babel/preset-react'
-        ]
-      }
+      return { presets }
     default:
-      return {
-        ignore: [
-          'src/**/*.test.js',
-          'src/setupTests.js'
-        ],
-        plugins: [
-          '@babel/plugin-transform-runtime',
-          '@babel/plugin-syntax-dynamic-import',
-          'styled-jsx/babel'
-        ],
-        presets: [
-          '@babel/preset-react'
-        ]
-      }
+      return { presets, ignore: ignoreTests }
   }
 }
