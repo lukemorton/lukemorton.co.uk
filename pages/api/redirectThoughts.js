48035ac505320ad4cc346ffa844c46770324523b
Luke Morton <lukemorton.dev@gmail.com>
2020-04-04 14:09:56 +0100

    Add redirectThoughts function

---
+++ pages/api/redirectThoughts.js
@@ -0,0 +1,14 @@
+export default (req, res) => {
+  let location
+
+  if (req.query.slug === 'archive') {
+    location = '/articles'
+  } else {
+    const slug = req.query.slug.slice('0000-00-00-'.length)
+    location = `/articles/${slug}`
+  }
+
+  res.status(302)
+  res.setHeader('Location', location)
+  res.end()
+}
