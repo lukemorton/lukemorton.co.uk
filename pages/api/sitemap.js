dd25c791fa426b8139f233ab2cd07b304be0e621
Luke Morton <lukemorton.dev@gmail.com>
2020-04-10 14:03:19 +0100

    Add sitemap.xml

---
+++ pages/api/sitemap.js
@@ -0,0 +1,47 @@
+import { SitemapStream, streamToPromise } from 'sitemap'
+import dependencyContainer from '../../src/app/dependencyContainer'
+
+const buildPages = async () => {
+  const { fetchAllThoughts, fetchAllTopics } = await dependencyContainer(
+    'build'
+  )
+
+  const staticPages = [
+    { url: '/', changefreq: 'weekly' },
+    { url: '/about', changefreq: 'weekly' },
+    { url: '/open-source', changefreq: 'weekly' },
+    { url: '/articles', changefreq: 'weekly' },
+  ]
+
+  const articlePages = (await fetchAllThoughts()).map(({ slug }) => {
+    return { url: `/articles/${slug}`, changefreq: 'weekly' }
+  })
+
+  const topicPages = (await fetchAllTopics()).map(({ slug }) => {
+    return { url: `/topics/${slug}`, changefreq: 'weekly' }
+  })
+
+  return [].concat(staticPages, articlePages, topicPages)
+}
+
+const writeSitemap = async () => {
+  const sitemap = new SitemapStream({ hostname: 'https://lukemorton.tech/' })
+  const pages = await buildPages()
+  pages.map((page) => sitemap.write(page))
+  return sitemap
+}
+
+const handleError = (context) => (e) => {
+  console.error(context, e)
+  exit(1)
+}
+
+export default async (req, res) => {
+  res.setHeader('Content-Type', 'application/xml')
+  res.on('error', handleError('res'))
+
+  const sitemap = await writeSitemap()
+  sitemap.on('error', handleError('sitemap'))
+  sitemap.pipe(res)
+  sitemap.end()
+}
