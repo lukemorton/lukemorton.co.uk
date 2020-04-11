1f72ce83a1acab80c539e1034ad3a2c610024ef1
Luke Morton <lukemorton.dev@gmail.com>
2020-04-11 15:54:41 +0100

    Move next.js pages into src

--- src/pageTests/topics/[slug].test.js
+++ src/pageTests/topics/[slug].test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import TopicPage from 'pages/topics/[slug]'
+import TopicPage from '../../pages/topics/[slug]'
 import Topic from 'src/components/Topics/Topic'
 import { fetchAllTopics } from 'src/factories/nodeFactory'
 
