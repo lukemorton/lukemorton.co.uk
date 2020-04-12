e84eb61b6b27921e7bb9899d9fa480e3a62475bb
Luke Morton <lukemorton.dev@gmail.com>
2020-04-12 14:27:02 +0100

    Use import with json-loader instead of fs

--- src/factories/nodeFactory/fetchThoughts.test.js
+++ src/factories/nodeFactory/fetchThoughts.test.js
@@ -1,4 +1,3 @@
-import fs from 'fs'
 import {
   fetchOneThoughtBySlug,
   fetchRecentThoughts,
@@ -10,57 +9,17 @@ import {
   NoThoughtsFoundByTopicSlugError,
 } from '../nodeFactory'
 
-jest.mock('fs')
-
 describe('listThoughts', () => {
-  let jsonResponse
-
-  beforeEach(() => {
-    fs.readFileSync.mockImplementation((_) => JSON.stringify(jsonResponse))
-  })
-
-  afterEach(() => {
-    jest.clearAllMocks()
-  })
-
   describe('.fetchOneThoughtBySlug()', () => {
-    beforeEach(() => {
-      jsonResponse = {} // thought returns object rather than array
-    })
-
-    test('it uses fs.readFileSync', async () => {
+    it('returns thought with matching slug', async () => {
       const expectedThought = {
         slug: 'lightweight-docker-images-for-go',
       }
-
-      jsonResponse = {
-        '2017-01-17-lightweight-docker-images-for-go': {
-          slug: '2017-01-17-lightweight-docker-images-for-go',
-        },
-      }
-
-      await fetchOneThoughtBySlug(null, expectedThought.slug)
-      expect(fs.readFileSync).toHaveBeenCalledWith(
-        expect.stringContaining('/dist/src/content/articles/index.json')
-      )
-    })
-
-    test('it returns thought with matching slug', async () => {
-      const expectedThought = {
-        slug: 'lightweight-docker-images-for-go',
-      }
-
-      jsonResponse = {
-        '2017-01-17-lightweight-docker-images-for-go': {
-          slug: '2017-01-17-lightweight-docker-images-for-go',
-        },
-      }
-
       const t = await fetchOneThoughtBySlug(null, expectedThought.slug)
       expect(t).toEqual(expectedThought)
     })
 
-    test('it raises exception if thought doesnt exist', async () => {
+    it('raises exception if thought doesnt exist', async () => {
       expect(fetchOneThoughtBySlug(null, 'doesnt-exist')).rejects.toThrow(
         NoThoughtFoundBySlugError
       )
@@ -68,77 +27,26 @@ describe('listThoughts', () => {
   })
 
   describe('.fetchRecentThoughts()', () => {
-    beforeEach(() => {
-      jsonResponse = []
-    })
-
-    test('it uses fs.readFileSync', async () => {
-      await fetchRecentThoughts(null)
-      expect(fs.readFileSync).toHaveBeenCalledWith(
-        expect.stringContaining('/dist/src/content/articles/recent.json')
-      )
-    })
-
-    test('it returns JSON response', async () => {
+    it('returns list of thoughts', async () => {
       const response = await fetchRecentThoughts(null)
-      expect(response).toEqual(jsonResponse)
-    })
-
-    test('it returns empty array when json response is null', async () => {
-      jsonResponse = null
-      const response = await fetchRecentThoughts(null)
-      expect(response).toEqual([])
+      expect(response.length).toEqual(1)
     })
   })
 
   describe('.fetchAllThoughts()', () => {
-    beforeEach(() => {
-      jsonResponse = []
-    })
-
-    test('it uses fs.readFileSync', async () => {
-      await fetchAllThoughts(null)
-      expect(fs.readFileSync).toHaveBeenCalledWith(
-        expect.stringContaining('/dist/src/content/articles/archive.json')
-      )
-    })
-
-    test('it returns JSON response', async () => {
+    it('returns list of thoughts', async () => {
       const response = await fetchAllThoughts(null)
-      expect(response).toEqual(jsonResponse)
-    })
-
-    test('it returns empty array when json response is null', async () => {
-      jsonResponse = null
-      const response = await fetchAllThoughts(null)
-      expect(response).toEqual([])
+      expect(response.length).toEqual(1)
     })
   })
 
   describe('.fetchThoughtsByTopicName()', () => {
-    beforeEach(() => {
-      jsonResponse = []
-    })
-
-    test('it uses fs.readFileSync', async () => {
-      await fetchThoughtsByTopicName(null, 'Ruby on Rails')
-      expect(fs.readFileSync).toHaveBeenCalledWith(
-        expect.stringContaining('/dist/src/content/articles/topics/rails.json')
-      )
-    })
-
-    test('it returns JSON response', async () => {
+    it('returns list of thoughts', async () => {
       const response = await fetchThoughtsByTopicName(null, 'Ruby on Rails')
-      expect(response).toEqual(jsonResponse)
+      expect(response.length).toEqual(1)
     })
 
-    test('it returns empty array when json response is null', async () => {
-      jsonResponse = null
-      const response = await fetchThoughtsByTopicName(null, 'Ruby on Rails')
-      expect(response).toEqual([])
-    })
-
-    test('it raises exception if topic doesnt exist', async () => {
+    it('raises exception if topic doesnt exist', async () => {
       expect(fetchThoughtsByTopicName(null, 'Jim Bob')).rejects.toThrow(
         NoThoughtsFoundByTopicNameError
       )
@@ -146,29 +54,12 @@ describe('listThoughts', () => {
   })
 
   describe('.fetchThoughtsByTopicSlug()', () => {
-    beforeEach(() => {
-      jsonResponse = []
-    })
-
-    test('it uses fs.readFileSync', async () => {
-      await fetchThoughtsByTopicSlug(null, 'rails')
-      expect(fs.readFileSync).toHaveBeenCalledWith(
-        expect.stringContaining('/dist/src/content/articles/topics/rails.json')
-      )
-    })
-
-    test('it returns JSON response', async () => {
+    it('returns list of thoughts', async () => {
       const response = await fetchThoughtsByTopicSlug(null, 'rails')
-      expect(response).toEqual(jsonResponse)
+      expect(response.length).toEqual(1)
     })
 
-    test('it returns empty array when json response is null', async () => {
-      jsonResponse = null
-      const response = await fetchThoughtsByTopicSlug(null, 'rails')
-      expect(response).toEqual([])
-    })
-
-    test('it raises exception if topic doesnt exist', async () => {
+    it('raises exception if topic doesnt exist', async () => {
       expect(fetchThoughtsByTopicSlug(null, 'jimbob')).rejects.toThrow(
         NoThoughtsFoundByTopicSlugError
       )
---
+++ src/mocks/markedlyIndex.json.js
@@ -0,0 +1,5 @@
+module.exports = {
+  '2017-01-17-lightweight-docker-images-for-go': {
+    slug: '2017-01-17-lightweight-docker-images-for-go',
+  },
+}
