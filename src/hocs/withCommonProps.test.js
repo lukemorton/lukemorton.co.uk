758cd942e2c1492ecabcc1e39f4c2cbb0f4a70ff
Luke Morton <lukemorton.dev@gmail.com>
2020-04-12 15:49:05 +0100

    Keep it react

--- src/hocs/withCommonProps.test.js
+++ src/hocs/withCommonProps.test.js
@@ -1,26 +1,26 @@
+import { shallow } from 'enzyme'
 import withCommonProps from './withCommonProps'
 import COMMON_PROPS from './commonProps'
 import AppContainer from '../containers/AppContainer'
 
 describe('withCommonProps', () => {
+  const MockComponent = () => <div />
+  const MockComponentWithCommonProps = withCommonProps(MockComponent)
+
   it('passes COMMON_PROPS as props', () => {
-    const MockComponent = jest.fn()
-    withCommonProps(MockComponent)()
-    expect(MockComponent).toBeCalledWith(expect.objectContaining(COMMON_PROPS))
+    const component = shallow(<MockComponentWithCommonProps />)
+    expect(component.props()).toEqual(expect.objectContaining(COMMON_PROPS))
   })
 
   it('passes COMMON_PROPS before other props', () => {
-    const MockComponent = jest.fn()
-    withCommonProps(MockComponent)({ indexUrl: '/?diff' })
-    expect(MockComponent).toBeCalledWith(
-      expect.objectContaining({ indexUrl: '/?diff' })
-    )
+    const otherProps = { indexUrl: '/?diff' }
+    const component = shallow(<MockComponentWithCommonProps {...otherProps} />)
+    expect(component.props()).toEqual(expect.objectContaining(otherProps))
   })
 
   it('passes container in props', () => {
-    const MockComponent = jest.fn()
-    withCommonProps(MockComponent)()
-    expect(MockComponent).toBeCalledWith(
+    const component = shallow(<MockComponentWithCommonProps />)
+    expect(component.props()).toEqual(
       expect.objectContaining({ container: expect.any(AppContainer) })
     )
   })
