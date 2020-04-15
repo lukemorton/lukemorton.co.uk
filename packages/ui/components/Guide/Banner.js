0f8041be9fd95867afebdf54832b11c4c91b146c
Luke Morton <lukemorton.dev@gmail.com>
2020-04-15 08:14:57 +0100

    Add GuideBanner

---
+++ packages/ui/components/Guide/Banner.js
@@ -0,0 +1,15 @@
+export const GuideBanner = ({ children }) => (
+  <>
+    <style jsx>{`
+      .banner {
+        border-top: 5px solid hsl(163, 100%, 50%);
+        background: hsl(163, 75%, 95%);
+        line-height: 1.5em;
+        margin-bottom: 1em;
+        padding: 0.4em;
+      }
+    `}</style>
+
+    <div className="banner">{children}</div>
+  </>
+)
