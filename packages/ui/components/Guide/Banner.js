5c3789bb5d6f2ade920b68d02864c091569fb699
Luke Morton <lukemorton.dev@gmail.com>
2020-05-09 18:00:54 +0100

    Add more spacing below guide banner

--- packages/ui/components/Guide/Banner.js
+++ packages/ui/components/Guide/Banner.js
@@ -5,7 +5,7 @@ export const GuideBanner = ({ children }) => (
         border-top: 5px solid hsl(163, 100%, 50%);
         background: hsl(163, 75%, 95%);
         line-height: 1.5em;
-        margin-bottom: 1em;
+        margin-bottom: 2em;
         padding: 0.4em;
       }
     `}</style>
