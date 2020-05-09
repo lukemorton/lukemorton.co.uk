d77ae89736219f318571744fcb6a5f737b5feb1d
Luke Morton <lukemorton.dev@gmail.com>
2020-05-09 13:02:20 +0100

    Add twitter conversion tracking

--- packages/ui/components/Page/Tracking/index.js
+++ packages/ui/components/Page/Tracking/index.js
@@ -1,6 +1,24 @@
-import React from 'react'
 import Router from 'next/router'
 import ga from 'react-ga'
+
+function twitterInit() {
+  !(function (e, t, n, s, u, a) {
+    e.twq ||
+      ((s = e.twq = function () {
+        s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments)
+      }),
+      (s.version = '1.1'),
+      (s.queue = []),
+      (u = t.createElement(n)),
+      (u.async = !0),
+      (u.src = '//static.ads-twitter.com/uwt.js'),
+      (a = t.getElementsByTagName(n)[0]),
+      a.parentNode.insertBefore(u, a))
+  })(window, document, 'script')
+
+  twq('init', 'o3pmi')
+  twq('track', 'PageView')
+}
 
 function logPageView(url) {
   ga.set({ page: url })
@@ -9,6 +27,7 @@ function logPageView(url) {
 
 if (process.env.NODE_ENV === 'production') {
   if (typeof window !== 'undefined') {
+    twitterInit()
     ga.initialize('UA-9371957-1')
     logPageView(window.location.pathname)
   }
@@ -16,4 +35,4 @@ if (process.env.NODE_ENV === 'production') {
   Router.onRouteChangeComplete = logPageView
 }
 
-export default () => <div />
+export default () => null
