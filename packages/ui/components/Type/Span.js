04924614b7d5c059ecc16c34b0f92a72c79719ce
Luke Morton <lukemorton.dev@gmail.com>
2020-05-17 14:24:01 +0100

    Add largeLineHeight option to TypeSpan

--- packages/ui/components/Type/Span.js
+++ packages/ui/components/Type/Span.js
@@ -1,11 +1,12 @@
 import { fromLarge } from '../../settings/breakpoints'
 
-export const TypeSpan = ({ largeFontSize, children }) => (
+export const TypeSpan = ({ largeFontSize, largeLineHeight, children }) => (
   <>
     <style jsx>{`
       @media ${fromLarge} {
         .type {
           font-size: ${largeFontSize};
+          line-height: ${largeLineHeight || 'inherit'};
         }
       }
     `}</style>
