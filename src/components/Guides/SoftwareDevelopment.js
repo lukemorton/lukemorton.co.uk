7ad0b26074e17a6c4eb8e209f41804a4ea93a129
Luke Morton <lukemorton.dev@gmail.com>
2020-08-28 08:54:22 +0100

    Add Roles in a software development team to roadmap

--- src/components/Guides/SoftwareDevelopment.js
+++ src/components/Guides/SoftwareDevelopment.js
@@ -72,6 +72,7 @@ export default () => (
             >
               <a>What is a software development team?</a>
             </Link>,
+            <span>🚧 Roles in a software development team</span>,
             <Link
               href="/articles/[slug]"
               as="/articles/every-team-helps-to-develop-software"
